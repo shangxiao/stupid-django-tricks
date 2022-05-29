d1c0837371bf9938c5ca6c3180e290659ffab0d3
David Sanders <>
2022-05-29 18:35:30 +1000

    Setup dj-database-url

--- stupid_django_tricks/settings.py
+++ stupid_django_tricks/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,10 +81,9 @@ WSGI_APPLICATION = "stupid_django_tricks.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="postgres://localhost/stupid_django_tricks"
+    ),
 }
 
 
